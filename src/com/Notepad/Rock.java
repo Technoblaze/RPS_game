4b4de505223aae41d74a254c26124fba83d0be00
ccadmin <ccadmin@DC-TECH-18.local>
2020-10-30 22:37:20 -0500

    merge all

--- src/com/Notepad/Rock.java
+++ src/com/Notepad/Rock.java
@@ -2,3 +2,32 @@ package com.Notepad;
 
 public class Rock {
 }
+public class Rock {
+
+    String cpuAnswer;
+
+
+    public Scissors(int choice) {
+        switch (choice) {
+            case 1:
+                cpuAnswer = "rock";
+                break;
+            case 2:
+                cpuAnswer = "paper";
+                break;
+            case 3:
+                cpuAnswer = "scissors";
+                break;
+            default:
+                System.out.println("Invalid choice");
+        }
+    }
+
+    public int checkOutcome() {
+        if(cpuAnswer.equals("paper")){
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+}
