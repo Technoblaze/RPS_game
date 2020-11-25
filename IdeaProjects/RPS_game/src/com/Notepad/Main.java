0964ac9a079b49efc3d5518fc735ac9226052a11
ccadmin <ccadmin@DC-TECH-18.local>
2020-11-25 16:29:48 -0600

    RPS_game

---
+++ IdeaProjects/RPS_game/src/com/Notepad/Main.java
@@ -0,0 +1,11 @@
+package com.Notepad;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Game myGame = new Game();
+
+System.out.println(myGame.startGame());
+    }
+}
